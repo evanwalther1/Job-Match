accd614d6ca028c372eba02946a8d8a8f2cd4761
evanwalther1 <evan.walther@my.wheaton.edu>
2025-02-04 15:02:55 -0600

    "evan"

--- src/components/ListGroupEvan.tsx
+++ src/components/ListGroupEvan.tsx
@@ -6,6 +6,7 @@ function ListGroupEvan() {
     <>
       <h1>List</h1>
       {items.length === 0 ? <p>No item Found</p> : null}
+      {items.length === 0 && <p>No item Found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
