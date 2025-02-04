0a949fe512e7a20b07049453ac873a33bc98de96
RachelChen129 <liying.chen@my.wheaton.edu>
2025-02-04 15:06:00 -0600

    itMerge branch 'main' of https://github.com/evanwalther1/Job-Match

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import MessageEvan from "./MessageEvan";
 
-<<<<<<< HEAD
-
-function App(){
-  return <div><MessageEvan></MessageEvan></div>
-=======
 function App() {
   return (
     <div>
       <MessageEvan></MessageEvan>
     </div>
   );
->>>>>>> 5f9e14e446276831ad1a3a12243fa0ddd51b06cc
 }
 
 export default App;
