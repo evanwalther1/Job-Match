d6bd952915b80c183659d59e384704068f23dbbc
evanwalther1 <evan.walther@my.wheaton.edu>
2025-02-04 22:51:29 -0600

    Did a lot of work, everything commited is the whole video worth of content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import MessageEvan from "./MessageEvan";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroupEvan from "./components/ListGroupEvan";
 
 function App() {
+  let items = ["New York", "San Francisco", "Chicago"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  const [cannotSee, canSee] = useState(false);
+
   return (
     <div>
-      <ListGroupEvan></ListGroupEvan>
+      {cannotSee && (
+        <Alert onClose={() => canSee(false)}>
+          Wow Look at you go you pressed a button!
+        </Alert>
+      )}
+      <Button color="secondary" onClick={() => canSee(true)}>
+        My Button
+      </Button>
+
+      <ListGroupEvan
+        items={items}
+        heading="cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
--- src/components/ListGroupEvan.tsx
+++ src/components/ListGroupEvan.tsx
@@ -1,13 +1,39 @@
-function ListGroupEvan() {
-  let items = ["New York", "San Francisco", "Chicago"];
+import { useState } from "react";
+
+// {items: [], heading: string}
+interface Props {
+  items: string[];
+  heading: string;
+  // (item: string) => void
+  onSelectItem: (item: string) => void; //onClick
+}
+
+function ListGroupEvan({ items, heading, onSelectItem }: Props) {
+  //hook: lets us tap into built in features in react
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  //event handler, handles the click event, also type specification is important here
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 ? <p>No item Found</p> : null}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
