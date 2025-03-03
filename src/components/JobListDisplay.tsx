f854c07a1d984475a470ede81a6c60c55e285f52
Eli Hamer <eli.hamer33@gmail.com>
2025-03-03 16:59:19 -0600

    created a couple of files

---
+++ src/components/JobListDisplay.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Job } from "../FirebaseServices";
+
+const JobListDisplay = ({}: Job) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default JobListDisplay;
