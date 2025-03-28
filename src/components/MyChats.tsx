4fe45758f2722d29372027e6c821aab8db6169f7
Fiona Kriner <fiona.kriner@my.wheaton.edu>
2025-03-28 00:07:45 -0500

    small changes to temporary stuff in the chats page

--- src/components/MyChats.tsx
+++ src/components/MyChats.tsx
@@ -32,18 +32,16 @@ const MyChats = () => {
     fetchJobs();
   }, []);
 
-  //<div class="card" style="width: 18rem;">
   return (
-    <div className="card">
+    <div className="card" style={{ width: "18rem" }}>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
+        <h6 className="card-subtitle mb-2 text-body-secondary">
+          From: SenderDisplayName
+        </h6>
         <p className="card-text">
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
       </div>
     </div>
   );
