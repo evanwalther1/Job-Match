091b9ba23359aeefe87f78be2e18e4a1427405ce
evanwalther1 <evan.walther@my.wheaton.edu>
2025-03-30 19:05:01 -0500

    fixed things

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,10 @@ import { useState, useEffect } from "react";
 import { db, auth } from "../firebase";
 import { Auth, signOut } from "firebase/auth";
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
-import profilePic from "../assets/profilepic.png";
 import { Link } from "react-router-dom";
 import { doc, onSnapshot } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
-import styles from "/src/css.styles/ActiveJobs.module.css";
+import styles from "/src/css.styles/Navbar.module.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -93,7 +92,11 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className={styles.navbtn} to="/profile">
+                <Link
+                  style={{ marginLeft: "10px" }}
+                  className={styles.navbtn}
+                  to="/profile"
+                >
                   <img
                     src={getProfilePic()}
                     alt="Profile"
@@ -113,7 +116,7 @@ const Navbar = () => {
                   aria-current="page"
                   to="/home"
                   className={styles.navbtn}
-                  style={{ marginRight: "10px", marginLeft: "10px" }}
+                  style={{ marginRight: "50px", marginLeft: "30px" }}
                 >
                   Home
                 </Link>
@@ -123,7 +126,7 @@ const Navbar = () => {
                   className={styles.navbtn}
                   aria-current="page"
                   to="/active-jobs"
-                  style={{ marginRight: "10px", marginLeft: "10px" }}
+                  style={{ marginRight: "50px", marginLeft: "10px" }}
                 >
                   Active Jobs
                 </Link>
@@ -133,7 +136,7 @@ const Navbar = () => {
                   className={styles.navbtn}
                   aria-current="page"
                   to="/job-history"
-                  style={{ marginRight: "10px", marginLeft: "10px" }}
+                  style={{ marginRight: "50px", marginLeft: "10px" }}
                 >
                   History
                 </Link>
@@ -143,7 +146,7 @@ const Navbar = () => {
                   className={styles.navbtn}
                   aria-current="page"
                   to="/chat"
-                  style={{ marginRight: "10px", marginLeft: "10px" }}
+                  style={{ marginRight: "50px", marginLeft: "10px" }}
                 >
                   Chat
                 </Link>
@@ -151,7 +154,7 @@ const Navbar = () => {
               <li className="nav-item">
                 <button
                   className={styles.logoutbtn}
-                  style={{ marginRight: "10px" }}
+                  style={{ marginLeft: "30px" }}
                   onClick={logOut}
                 >
                   Logout
