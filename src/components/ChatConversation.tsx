78131e97ade25ec5cdca4582edd36618f8f7f216
Fiona Kriner <fiona.kriner@my.wheaton.edu>
2025-04-28 13:27:09 -0500

    make chat real time

--- src/components/ChatConversation.tsx
+++ src/components/ChatConversation.tsx
@@ -1,10 +1,12 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { auth } from "../firebase";
 import {
   ChatMessage,
   getMessagesFromOneUserToAnother,
+  getUser,
 } from "../FirebaseServices";
 import ChatBubble from "./ChatBubble";
+import { Timestamp } from "firebase/firestore";
 
 interface Props {
   otherUserID: string;
@@ -38,6 +40,7 @@ const ChatConversation = ({ otherUserID }: Props) => {
   >([]);
   const [messagesStored, setMessagesStored] = useState<string[]>([]);
   const [returnElement, setReturnElement] = useState<ReactNode>(null);
+  const [otherUserName, setOtherUserName] = useState<string>("");
 
   const grabMessagesAndSort = async () => {
     console.log("grabMessagesAndSort - start");
@@ -54,7 +57,7 @@ const ChatConversation = ({ otherUserID }: Props) => {
           messagesStored.length
         );
         messagesStored.push(value.id);
-        messagesWithRightAlignData.push([value, false]);
+        messagesWithRightAlignData.push([value, true]);
       }
     });
     messagesSentByOtherUser.forEach((value) => {
@@ -65,7 +68,7 @@ const ChatConversation = ({ otherUserID }: Props) => {
           messagesStored.length
         );
         messagesStored.push(value.id);
-        messagesWithRightAlignData.push([value, true]);
+        messagesWithRightAlignData.push([value, false]);
       }
     });
     console.log(
@@ -90,17 +93,37 @@ const ChatConversation = ({ otherUserID }: Props) => {
     );
   };
 
-  console.log("line 93");
-  grabMessagesAndSort();
-  console.log("line 95");
-  //turnMessagesToUI();
+  const getOtherUserName = async () => {
+    const otherUser = await getUser(otherUserID);
+    //CURRENT PROBLEM: THE GETUSER() CALLS ARE RETURNING USER DOES NOT EXIST
+    /*console.log("entering while loop - hold tight");
+    while (otherUser == null) {
+      //kill time
+    }
+    console.log("exiting while loop");*/
+    if (otherUser == null) {
+      console.log("the other user call is null; returning");
+      return;
+    }
+    setOtherUserName(otherUser.displayName);
+  };
+
+  useEffect(() => {
+    grabMessagesAndSort();
+    getOtherUserName();
+    turnMessagesToUI();
+  });
 
   return (
     <div className="card">
       {returnElement}
       <div className="card">
-        <div className="card"></div>
-        <div className="card"></div>
+        <div className="card">
+          <p>{otherUserName == "" ? "Them" : otherUserName}</p>
+        </div>
+        <div className="card text-end">
+          <p>{"You"}</p>
+        </div>
       </div>
       <button
         onClick={() => {
